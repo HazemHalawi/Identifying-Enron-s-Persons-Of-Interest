1d2eb12af2b6bd2063269e03faaee1a4b83094c4
HazemHalawi <Hazem.Halawi@iase.edu.lb>
2018-04-27 11:41:00 +0300

    Selecting best parameter tune with GridSearchCV

--- decision-tree.py
+++ decision-tree.py
@@ -36,3 +36,21 @@ features_test  = vectorizer.transform(features_test).toarray()
 vocabulary = vectorizer.get_feature_names()
 
 
+
+########################Training###########################
+from sklearn import tree
+from sklearn.model_selection import GridSearchCV
+
+###The section below is used for Parameter tuning
+param_grid = {'min_samples_split': [2, 3, 4, 5, 10, 20, 40, 50, 100] }
+clf = GridSearchCV(tree.DecisionTreeClassifier(), param_grid)
+
+#clf = tree.DecisionTreeClassifier()#min_samples_split=40)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+print("Best estimator found by grid search:")
+print(clf.best_estimator_)
+
+
