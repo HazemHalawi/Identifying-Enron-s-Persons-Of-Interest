50ca2ec14ecc2597539c9cc99b7d981705e8e968
HazemHalawi <Hazem.Halawi@iase.edu.lb>
2018-04-27 10:45:08 +0300

    Splitting Data with 20% test set

--- decision-tree.py
+++ decision-tree.py
@@ -6,6 +6,7 @@ np.random.seed(42)
 from time import time
 
 
+
 ########################Load Data#########################
 ### The words (features) and authors (labels), already largely processed.
 words_file = "word_data.pkl" 
@@ -14,6 +15,15 @@ word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
 
 
+
+####################Splitting Data########################
+### test_size is the percentage of events assigned to the test set (the
+### remainder go into training)
+### feature matrices changed to dense representations for compatibility with
+### classifier functions in versions 0.15.2 and earlier
+from sklearn import cross_validation
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.2, random_state=42)
+print len(features_train), len(features_test)
 
 
 
